7387607938ef2ca3efee4fed23785bed5afa0795
lorenzocravero <62762272+lorenzocravero@users.noreply.github.com>
2020-11-03 16:07:10 +0100

    Add files via upload

---
+++ 008-Singleton con coda/Esercizio 4 da solo/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Esercizio_4_da_solo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnStampa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserire elemento nella coda";
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.Location = new System.Drawing.Point(15, 42);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(104, 23);
+            this.btnInserisci.TabIndex = 1;
+            this.btnInserisci.Text = "Inserisci in coda";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.BtnInserisci_Click);
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(167, 6);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(100, 20);
+            this.txtElemento.TabIndex = 2;
+            // 
+            // btnStampa
+            // 
+            this.btnStampa.Location = new System.Drawing.Point(15, 71);
+            this.btnStampa.Name = "btnStampa";
+            this.btnStampa.Size = new System.Drawing.Size(104, 23);
+            this.btnStampa.TabIndex = 3;
+            this.btnStampa.Text = "Stampa elemento";
+            this.btnStampa.UseVisualStyleBackColor = true;
+            this.btnStampa.Click += new System.EventHandler(this.BtnStampa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 235);
+            this.Controls.Add(this.btnStampa);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.btnInserisci);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnStampa;
+    }
+}
+
---
+++ 008-Singleton con coda/Esercizio 4 da solo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esercizio_4_da_solo
+{
+    public partial class Form1 : Form
+    {
+        public string item;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInserisci_Click(object sender, EventArgs e)
+        {
+            item = txtElemento.Text;
+            Queue.inserisci(item);
+            txtElemento.Text = "";
+            txtElemento.Focus();
+        }
+
+        private void BtnStampa_Click(object sender, EventArgs e)
+        {
+            string s = Queue.estrai(item);
+            MessageBox.Show(s);
+        }
+    }
+}
