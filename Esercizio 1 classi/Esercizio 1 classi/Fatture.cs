5bf02b4a56f11a8c712b3adc2c7923c76dcc3b3a
lorenzocravero <62762272+lorenzocravero@users.noreply.github.com>
2020-11-03 16:08:43 +0100

    Add files via upload

---
+++ Esercizio 1 classi/Esercizio 1 classi/Fatture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1_classi
+{
+    class Fatture
+    {
+        private static int cntFatture = 0;
+        public readonly int nFattura;
+
+        public Fatture()
+        {
+            cntFatture++;
+            this.nFattura = cntFatture;
+        }
+
+        public static void visualizza()
+        {
+            System.Windows.Forms.MessageBox.Show("Numero di fatture create: "+cntFatture);
+            //System.Windows.Forms.MessageBox.Show("Numero di fattura corrente: "+nFattura);
+        }
+    }
+}
---
+++ Esercizio 1 classi/Esercizio 1 classi/MiaMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1_classi
+{
+    static class MiaMath
+    {
+        public static double pi = 3.14;
+        public static double prodotto(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
