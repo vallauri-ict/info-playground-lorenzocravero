e9a99b03af87bdf67ab9a621b23187e18ab0528d
lorenzocravero <62762272+lorenzocravero@users.noreply.github.com>
2020-12-01 19:05:36 +0100

    Add files via upload

---
+++ Classi derivate/Classi derivate/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classi_derivate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Mostro demone = new Mostro();
+            MessageBox.Show(demone.Valore()+" "+demone.Verso());
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Mannaro umano = new Mannaro();
+            MessageBox.Show(umano.Valore() + " " + umano.Verso());
+            umano.Muta();
+            MessageBox.Show(umano.Valore() + " " + umano.Verso());
+        }
+    }
+}
---
+++ Classi derivate/Classi derivate/Mannaro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi_derivate
+{
+    class Mannaro : Mostro
+    {
+        protected bool mutato;
+        public Mannaro()
+        {
+            punti = 33;
+            verso = "ululato";
+            mutato = true;
+        }
+
+        public double Valore()
+        {
+            if (mutato)
+                return 2 * base.Valore();
+            else
+                return base.Valore();
+        }
+
+        public string Verso()
+        {
+            if (mutato)
+                return verso;
+            else
+                return base.verso;
+        }
+
+        public void Muta()
+        {
+            mutato = !mutato;
+        }
+    }
+}
