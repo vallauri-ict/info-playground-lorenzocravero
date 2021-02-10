2d59682f7148253bf62069ec5c4d07b4237109a2
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-02-10 15:20:54 +0100

    Primi 2 esercizi sulle interfacce

---
+++ Interfacce-1/Interfacce-1/IDispAggiuntivo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacce_1
+{
+    interface IDispAggiuntivo
+    {
+        int id { get; set; }
+
+        bool status { get; }
+
+        void connetti(string s);
+
+        void disconnetti(string s);
+    }
+}
---
+++ Interfacce-2/Interfacce-2/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacce_2
+{
+    class Persona : IComparable
+    {
+        private string nome;
+        private string cognome;
+        private string citta;
+
+        public Persona(string nome, string cognome, string citta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Citta = citta;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Citta { get => citta; set => citta = value; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1; //un oggetto null è più piccolo 
+            Persona p = obj as Persona;
+            return String.Compare(Cognome, p.Cognome, true); //true per il case sensitive
+        }
+
+        public override string ToString()
+        {
+            return Cognome + " " + Nome + " " + Citta;
+        }
+    }
+}
