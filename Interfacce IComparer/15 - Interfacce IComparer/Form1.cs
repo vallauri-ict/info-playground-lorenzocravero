aee1213bf158353c28f72e63352a09b7edb15a5c
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-02-24 09:00:00 +0100

    Esercizio interfacce comparabili finito

---
+++ Interfacce IComparer/15 - Interfacce IComparer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15___Interfacce_IComparer
+{
+    public partial class Form1 : Form
+    {
+        Persona[] vet = {new Persona("Luca", "Ariaudo", "Fossano"),
+                        new Persona("Samuele","De Toffol","Bra"),
+                        new Persona("Andrea","Abbate","Alba"),
+                        new Persona("Christian","Gallone","Fossano")};
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowPersona();
+        }
+
+        private void BtnCognome_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vet, new Persona.ConfrontaCognome());
+            ShowPersona();
+        }
+
+        private void BtnNome_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vet, new Persona.ConfrontaNome());
+            ShowPersona();
+        }
+
+        public void ShowPersona()
+        {
+            string s = "";
+            foreach (var persona in vet)
+            {
+                s += persona.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+    }
+}
---
+++ Interfacce IComparer/15 - Interfacce IComparer/Persona.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15___Interfacce_IComparer
+{
+    class Persona
+    {
+        private string nome;
+        private string cognome;
+        private string citta;
+
+        public Persona(string nome, string cognome, string citta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Citta = citta;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Citta { get => citta; set => citta = value; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1; //un oggetto null è più piccolo 
+            Persona p = obj as Persona;
+            return String.Compare(Cognome, p.Cognome, true); //true per il case sensitive
+        }
+
+        public override string ToString()
+        {
+            return Cognome + " " + Nome + " " + Citta;
+        }
+
+        public class ConfrontaCognome : IComparer<Object>
+        {
+            public int Compare(object x, object y)
+            {
+                if (x == null && y == null) return 0;
+                else if (x == null) return 1;
+                else if (y == null) return -1;
+                else
+                {
+                    Persona p1 = (Persona)x;
+                    Persona p2 = (Persona)y;
+                    return String.Compare(p1.Cognome, p2.Cognome, true);
+                }
+            }
+        }
+
+        public class ConfrontaNome : IComparer<Object>
+        {
+            public int Compare(object x, object y)
+            {
+                if (x == null && y == null) return 0;
+                else if (x == null) return 1;
+                else if (y == null) return -1;
+                else
+                {
+                    Persona p1 = (Persona)x;
+                    Persona p2 = (Persona)y;
+                    return String.Compare(p1.Nome, p2.Nome, true);
+                }
+            }
+        }
+    }
+}
