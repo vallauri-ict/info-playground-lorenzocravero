3315ef0b6a1240a74bd1a8c483ee9612b5d29836
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-02-26 09:37:28 +0100

    Esercizio 17 sui delegati

---
+++ PrimoEsDispenseMana-Delegate/PrimoEsDispenseMana-Delegate/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimoEsDispenseMana_Delegate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalva_Click(object sender, EventArgs e)
+        {
+            GestoreSalvataggio gest = new GestoreSalvataggio();
+            gest.salvataggioEseguito += new SalvataggioEseguitoEventHandler(miaFunzione);
+            gest.salvataggioEseguito += new SalvataggioEseguitoEventHandler(miaFunzione2);
+            gest.SalvaSuFile(5);
+        }
+
+        private void miaFunzione(int id)
+        {
+            MessageBox.Show("Elemento "+id+" salvato correttamente");
+        }
+
+        private void miaFunzione2(int id)
+        {
+            MessageBox.Show("Secondo gestore di event");
+        }
+    }
+}
