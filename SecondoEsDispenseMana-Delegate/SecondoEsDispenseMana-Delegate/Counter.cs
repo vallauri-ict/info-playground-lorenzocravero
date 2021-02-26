1c9b743e2fdbcd335cbe293e2e56423cb05a3c85
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-02-26 09:57:26 +0100

    Secondo es delegate in stato d'avanzamento

---
+++ SecondoEsDispenseMana-Delegate/SecondoEsDispenseMana-Delegate/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondoEsDispenseMana_Delegate
+{
+    public delegate void OverMaxEventHandler(object sender, OverMaxEventArgs e);
+    class Counter
+    {
+        private int maxVal;
+        public Counter(int n)
+        {
+            if(n>10)
+            {
+                throw new ArgumentException("Il valore massimo è 10");
+            }
+            else
+            {
+                maxVal = n;
+            }
+        }
+    }
+}
---
+++ SecondoEsDispenseMana-Delegate/SecondoEsDispenseMana-Delegate/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SecondoEsDispenseMana_Delegate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCrea = new System.Windows.Forms.Button();
+            this.btnIcrement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnCrea
+            // 
+            this.btnCrea.Location = new System.Drawing.Point(12, 56);
+            this.btnCrea.Name = "btnCrea";
+            this.btnCrea.Size = new System.Drawing.Size(86, 23);
+            this.btnCrea.TabIndex = 1;
+            this.btnCrea.Text = "Create";
+            this.btnCrea.UseVisualStyleBackColor = true;
+            this.btnCrea.Click += new System.EventHandler(this.BtnCrea_Click);
+            // 
+            // btnIcrement
+            // 
+            this.btnIcrement.Location = new System.Drawing.Point(12, 95);
+            this.btnIcrement.Name = "btnIcrement";
+            this.btnIcrement.Size = new System.Drawing.Size(86, 23);
+            this.btnIcrement.TabIndex = 2;
+            this.btnIcrement.Text = "Increment";
+            this.btnIcrement.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 161);
+            this.Controls.Add(this.btnIcrement);
+            this.Controls.Add(this.btnCrea);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCrea;
+        private System.Windows.Forms.Button btnIcrement;
+    }
+}
+
